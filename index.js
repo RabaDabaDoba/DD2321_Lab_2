b12dad3b448563b115e45665a69d4852ccbc79fb
RabaDabaDoba <rodah1997@gmail.com>
2020-02-03 13:57:01 +0100

    stuff

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+d3.select('body').style('background-color', 'white');
 
-d3.select('p')
-.style('color', 'red');
+d3.select('body').append('div')
+.attr('id', 'myDiv')
+.attr('width', '100px')
+.attr('height', '100px')
+.style('background-color', 'yellow');
+
+d3.select('body').append('div')
+.attr('class', 'myDiv')
+.attr('width', '100px')
+.attr('height', '100px')
+.style('background-color', 'red');
+
+d3.selectAll('div').append('p').text("asdasd").attr('style','font-size:20');
+
+d3.selectAll('.myDiv')
+.transition()
+.duration(1000)
+.style('background-color','cyan')
+
+var svg = d3.select("body")
+        .append("svg")
+        .attr("width", 500)
+        .attr("height", 500);
+
+var bar1 = svg.append('rect')
+        .attr("fill", "blue")
+        .attr("x", 100)
+        .attr("y", 20)
+        .attr("height", 20)
+        .attr("width", 10);
+
+update();
+
+function update(){
+  bar1.transition()
+  .ease(d3.easeLinear)
+  .duration(2000)
+  .attr("height",100)
+
+}
+
