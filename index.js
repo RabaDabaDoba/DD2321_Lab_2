22696494a90346f4681066d34ea4c73c50761466
RabaDabaDoba <rodah1997@gmail.com>
2020-02-04 11:10:09 +0100

    changes

--- index.js
+++ index.js
@@ -1,43 +1,64 @@
 d3.select('body').style('background-color', 'white');
 
 d3.select('body').append('div')
-.attr('id', 'myDiv')
-.attr('width', '100px')
-.attr('height', '100px')
-.style('background-color', 'yellow');
+  .attr('id', 'myDiv')
+  .attr('width', '100px')
+  .attr('height', '100px')
+  .style('background-color', 'yellow');
 
 d3.select('body').append('div')
-.attr('class', 'myDiv')
-.attr('width', '100px')
-.attr('height', '100px')
-.style('background-color', 'red');
+  .attr('class', 'myDiv')
+  .attr('width', '100px')
+  .attr('height', '100px')
+  .style('background-color', 'red');
 
-d3.selectAll('div').append('p').text("asdasd").attr('style','font-size:20');
+d3.selectAll('div').append('p').text("asdasd").attr('style', 'font-size:20');
 
 d3.selectAll('.myDiv')
-.transition()
-.duration(1000)
-.style('background-color','cyan')
+  .transition()
+  .duration(1000)
+  .style('background-color', 'cyan')
 
 var svg = d3.select("body")
-        .append("svg")
-        .attr("width", 500)
-        .attr("height", 500);
+  .append("svg")
+  .attr("width", 500)
+  .attr("height", 500);
 
-var bar1 = svg.append('rect')
-        .attr("fill", "blue")
-        .attr("x", 100)
-        .attr("y", 20)
-        .attr("height", 20)
-        .attr("width", 10);
 
-update();
+let myData = [1, 2, 3, 4, 5, 6];
+var bar1 = svg.append('circle')
 
-function update(){
+  .attr("fill", "blue")
+  .attr("cx", 100)
+  .attr("cy", 100)
+  .attr('r', 10)
+  .attr("height", 20)
+  .on('mouseover', function (d) {
+    if (d3.select(bar1).attr('r' == 50)) {
+      bar1.transition()
+        .ease(d3.easeLinear)
+        .duration(500)
+        .attr('r', 10)
+        .attr("fill", "cyan")
+        .attr('stroke', 'black');
+    } else {
+      update()
+    }
+  })
+  .attr("width", 10);
+
+  d3.select('body').append('h1').text();
+
+
+//update();
+
+function update() {
   bar1.transition()
-  .ease(d3.easeLinear)
-  .duration(2000)
-  .attr("height",100)
+    .ease(d3.easeLinear)
+    .duration(500)
+    .attr('r', 50)
+    .attr("fill", "cyan")
+    .attr('stroke', 'black');
 
 }
 
