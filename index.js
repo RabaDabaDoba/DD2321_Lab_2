922e9093ac063af922e0ed46dae1a03901985e61
RabaDabaDoba <rodah1997@gmail.com>
2020-02-11 10:50:31 +0100

    sum
    sdf

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-d3.select('body').style('background-color', 'white');
+/*d3.select('body').style('background-color', 'white');
 
 d3.select('body').append('div')
   .attr('id', 'myDiv')
@@ -62,3 +62,48 @@ function update() {
 
 }
 
+*/
+//interact with this variable from a javascript console
+var pc0;
+
+d3.csv('planets.csv', function(data) {
+pc0 = d3.parcoords()("#example0")
+  .data(data)
+	.bundlingStrength(0) // set bundling strength
+	.smoothness(0)
+	.bundleDimension("cylinders")
+	.showControlPoints(false)
+	.hideAxis(["name"])
+	.render()
+	.brushMode("1D-axes")
+	.reorderable()
+	.interactive();
+
+	// smoothness
+	d3.select("#smoothness").on("change", function() {
+		d3.select("#smooth").text(this.value);
+		pc0.smoothness(this.value).render();
+	});
+
+	// bundling strength slider
+	d3.select("#bundling").on("change", function() {
+		d3.select("#strength").text(this.value);
+		pc0.bundlingStrength(this.value).render();
+	});
+
+	var select = d3.select("#bundleDimension").append("select").on("change", changeBundle);
+
+	var options = select.selectAll('option')
+		.data(d3.keys(pc0.dimensions()));
+
+	options
+		.enter()
+		.append("option")
+		.attr("value", function(d) {return d;})
+		.text(function(d) {return d;});
+
+	function changeBundle() {
+		pc0.bundleDimension(this.value);
+	}
+
+});
